c58648e46cc9cc60a85f82233cab85f78e384220
Hongda Hu <hdhu@thoughtworks.com>
2014-07-12 16:10:34 +0800

    [jerry] modify the configuration

--- chef/site-cookbooks/server-basic/attributes/default.rb
+++ chef/site-cookbooks/server-basic/attributes/default.rb
@@ -1,10 +1,13 @@
-default['server-basic']['basic_directorys'] = [
-    '/data/pids',
-    '/data/logs/redis', '/data/logs/mysql', '/data/logs/mongodb', '/data/logs/nginx', '/data/logs/memcached', '/data/logs/monit',
-    '/data/data/redis', '/data/data/mysql', '/data/data/mongodb', '/data/data/monit'
-]
+default['server-basic']['users'] = {
+    "redis"     => ['/data/pids/redis', '/data/logs/redis', '/data/data/redis'],
+    'mysql'     => ['/data/pids/mysql', '/data/logs/mysql', '/data/data/mysql'],
+    'mongod'    => ['/data/pids/mongodb', '/data/logs/mongodb', '/data/data/mongodb'],
+    'www'       => ['/data/pids/nginx', '/data/logs/nginx'],
+    'memcached' => ['/data/pids/memcached', '/data/logs/memcached'],
+    'monit'     => ['/data/pids/monit', '/data/logs/monit', '/data/data/monit']
+}
 
 default['server-basic']['basic_package'] = [
-    "tree", "tar"
+    "tree", "tar", "wget"
 ]
 
--- chef/site-cookbooks/server-basic/recipes/default.rb
+++ chef/site-cookbooks/server-basic/recipes/default.rb
@@ -1,13 +1,27 @@
-node['server-basic']['basic_directorys'].each do |_dir|
-  directory _dir do
-    owner 'root'
-    group 'root'
-    mode '0755'
-    action :create
-    recursive true
-  end
-end
-
 node['server-basic']['basic_package'].each do |_package|
   package _package
+end
+
+node['server-basic']['users'].each do |_user, _dirs|
+  user _user do
+    comment "system #{_user}"
+    system true
+    shell "/bin/false"
+  end
+
+  group _user do
+    action :create
+    system  true
+  end
+
+  _dirs.each do |_dir|
+    directory _dir do
+      owner _user
+      group _user
+      mode '0755'
+      action :create
+      recursive true
+    end
+  end
+
 end
